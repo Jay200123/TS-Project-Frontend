995b29d7bec0bb7322119f6bba5659d08cefda44
Jay200123 <kenken.ayonon@gmail.com>
2024-11-15 22:20:46 +0800

    add authentication store using Zustand for user login and logout management

---
+++ src/state/authentication.ts
@@ -0,0 +1,55 @@
+import { create } from "zustand";
+import { encryptedSessionStorage } from "../storage";
+import axios from "axios";
+import { persist } from "zustand/middleware";
+import { AuthState } from "../interface";
+import { PATH } from "../constants";
+
+const useAuthenticationStore = create<AuthState>()(
+    persist((set) => ({
+        user: null,
+        loading: false,
+        error: null,
+        login: async (email, password) => {
+            const res = await axios.post(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}${PATH.USER_LOGIN}`,
+                {
+                    email,
+                    password
+                },
+            );
+
+            if (res.data.success === false) {
+                set(
+                    {
+                        user: null,
+                        loading: false,
+                        error: res.data.error.message,
+                    }
+                );
+            }
+            set(
+                {
+                    user: res.data.details,
+                    loading: false,
+                    error: null,
+                }
+            );
+
+        },
+        logout: async () => {
+            set(
+                {
+                    user: null,
+                    loading: false,
+                    error: null,
+                }
+            );
+            sessionStorage.removeItem("user-auth");
+        }
+    }), {
+        name: "user-auth",
+        storage: encryptedSessionStorage,
+    })
+);
+
+export default useAuthenticationStore;

fb4d1a3b5e76dfe6eab901ef528da806a217cf04
Jay200123 <kenken.ayonon@gmail.com>
2024-11-15 13:49:32 +0800

    update Footer component to add background color

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function () {
   return (
     <>
-      <footer className='w-full h-20'>
+      <footer className='w-full h-20 bg-slate-500'>
         <h3>hi this is a footer</h3>
       </footer>
     </>

00b8761f55a542b842303a5e8e90bddb95a18b15
Jay200123 <kenken.ayonon@gmail.com>
2024-11-15 22:20:31 +0800

    add authentication interfaces for user login and state management

---
+++ src/interface/authentication.ts
@@ -0,0 +1,16 @@
+import { User } from "./user";
+
+interface AuthenticationValues {
+    email: string;
+    password: string;   
+}
+
+interface AuthState {
+    user: User | null;
+    loading: boolean;
+    error: string | null;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+export type { AuthState, AuthenticationValues};

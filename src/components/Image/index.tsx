729bda31b2b43e9d4b3d18635377a004dbf20590
Jay200123 <kenken.ayonon@gmail.com>
2024-11-16 23:26:31 +0800

    Add Image component to display ticket image

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,11 @@
+import ticketImage from "../../assets/picture-1.jpeg";   
+
+export default function(){
+    return(
+        <>
+        <main>
+            <img className="h-full w-full object-cover rounded-l-lg" src={ticketImage}/>
+        </main>
+        </>
+    )
+}

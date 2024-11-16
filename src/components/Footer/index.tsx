1fbdd9f1c8cdd548fe7d0a17f750e41049e9ec49
Jay200123 <kenken.ayonon@gmail.com>
2024-11-16 14:48:29 +0800

    Update Footer component background color from slate-500 to gray-800

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function () {
   return (
     <>
-      <footer className='w-full h-20 bg-slate-500'>
+      <footer className='w-full h-20 bg-gray-800'>
         <h3>hi this is a footer</h3>
       </footer>
     </>

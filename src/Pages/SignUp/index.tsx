ffd2a31dbfa85a128bdcd7f2ce1fd6ccf70d70b9
Jay200123 <kenken.ayonon@gmail.com>
2024-11-16 23:26:03 +0800

    Add SignUp component with basic registration page structure

---
+++ src/Pages/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Formik } from "formik";
+
+export default function(){
+    return(
+        <>
+        <h1>this is registration page</h1>
+        </>
+    )
+}

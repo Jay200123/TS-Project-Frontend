dcad28c0905785707759b0cc05d683fbff581d3a
Jay200123 <kenken.ayonon@gmail.com>
2024-11-16 23:26:23 +0800

    Add Image component to index export for improved modularity

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ProtectedRoute from "./ProtectedRoutes";
+import Image from "./Image";
 
 export {
     Navbar,
     Footer,
-    ProtectedRoute
+    ProtectedRoute,
+    Image
 }

942952b7fa5bd82abbefa8ed322993e30c18498c
Jay200123 <kenken.ayonon@gmail.com>
2024-11-16 11:51:55 +0800

    Add Unauthorized page to index export

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,9 +1,11 @@
 import Home from './Home';
 import Login from './Login';
 import AuthenticatedTest from './AuthenticatedTest';
+import Unauthorized from './Unauthorized';
 
 export {
     Home,
     Login,
-    AuthenticatedTest
+    AuthenticatedTest,
+    Unauthorized
 }

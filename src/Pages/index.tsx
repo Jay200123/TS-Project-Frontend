7c253924fc65047ff94c4add0c0db478657f36ae
Jay200123 <kenken.ayonon@gmail.com>
2024-11-17 21:04:12 +0800

    Add Dashboard component to index export

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -3,11 +3,13 @@ import Login from './Login';
 import AuthenticatedTest from './AuthenticatedTest';
 import Unauthorized from './Unauthorized';
 import SignUp from './SignUp';
+import Dashboard from './Dashboard';
 
 export {
     Home,
     Login,
     AuthenticatedTest,
     Unauthorized,
-    SignUp
+    SignUp,
+    Dashboard   
 }

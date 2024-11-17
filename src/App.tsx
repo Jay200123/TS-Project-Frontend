878aaf242a420890d28ddfee17ed422b701357e4
Jay200123 <kenken.ayonon@gmail.com>
2024-11-17 21:04:42 +0800

    Refactor routing structure to implement separate layouts for Home, Customer, and Admin with respective protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,52 @@ import {
   createRoutesFromElements,
   RouterProvider
 } from 'react-router-dom'
-import { Home, Login, AuthenticatedTest, Unauthorized, SignUp } from './Pages'
+import {
+  Home,
+  Login,
+  AuthenticatedTest,
+  Unauthorized,
+  SignUp,
+  Dashboard
+} from './Pages'
 import { ProtectedRoute } from './components'
-import MainLayout from './layout'
+import { HomeLayout, AdminLayout, CustomerLayout } from './layout'
 
 function App () {
   const Router = createBrowserRouter(
     createRoutesFromElements(
-      <Route element={<MainLayout />}>
-        {/* Public Routes  */}
-        <Route index element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/unauthorized' element={<Unauthorized />} />
-        <Route path='/signup' element={<SignUp />} />
-
-        {/* Private Routes */}
-        <Route
-          path='/test'
-          element={
-            <ProtectedRoute userRole={['admin', 'customer']}>
-              <AuthenticatedTest />
-            </ProtectedRoute>
-          }
-        />
+      <Route>
+        <Route element={<HomeLayout />}>
+          {/* Public Routes  */}
+          <Route index element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/unauthorized' element={<Unauthorized />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Route>
+        {/* Customer Private Routes */}
+        <Route element={<CustomerLayout />}>
+          <Route
+            path='/test'
+            index
+            element={
+              <ProtectedRoute userRole={['customer']}>
+                <AuthenticatedTest />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        {/* Admin Private Routes */}
+        <Route element={<AdminLayout />}>
+          <Route
+            path='/dashboard'
+            index
+            element={
+              <ProtectedRoute userRole={['admin']}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
       </Route>
     )
   )

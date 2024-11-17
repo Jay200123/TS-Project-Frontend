4938d0016942859c50051529dd4ed4f1fd2f1e45
Jay200123 <kenken.ayonon@gmail.com>
2024-11-17 16:51:04 +0800

    Add user store import and prepare for fetching all users in AuthenticatedTest component

--- src/Pages/AuthenticatedTest/index.tsx
+++ src/Pages/AuthenticatedTest/index.tsx
@@ -1,7 +1,15 @@
-import { useAuthenticationStore } from '../../state/store'
+import { useAuthenticationStore, useUserStore } from '../../state/store'
+import { useEffect } from 'react';
 
 export default function(){
     const { user } = useAuthenticationStore();
+    // const { users, getAllUsers } = useUserStore();  
+    
+    // useEffect(() => {
+    //     getAllUsers();
+    // }, [getAllUsers]);
+
+    // console.log(users);
 
     if(!user){
         return <h3>Not authenticated! :(</h3>

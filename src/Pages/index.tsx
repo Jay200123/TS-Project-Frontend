80354f9da461ef7ecbc99975464e33f488442b56
Jay200123 <kenken.ayonon@gmail.com>
2024-11-17 11:34:31 +0800

    Add SignUp component to index export for improved modularity

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -2,10 +2,12 @@ import Home from './Home';
 import Login from './Login';
 import AuthenticatedTest from './AuthenticatedTest';
 import Unauthorized from './Unauthorized';
+import SignUp from './SignUp';
 
 export {
     Home,
     Login,
     AuthenticatedTest,
-    Unauthorized
+    Unauthorized,
+    SignUp
 }

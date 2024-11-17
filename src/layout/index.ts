8b9d39c82567b263de876f4814387a0ee6c67249
Jay200123 <kenken.ayonon@gmail.com>
2024-11-17 21:04:17 +0800

    Add layout exports for Admin, Customer, and Home components

---
+++ src/layout/index.ts
@@ -0,0 +1,9 @@
+import AdminLayout from "./AdminLayout";
+import CustomerLayout from "./CustomerLayout";
+import HomeLayout from "./HomeLayout";
+
+export {
+    AdminLayout,
+    CustomerLayout,
+    HomeLayout
+}

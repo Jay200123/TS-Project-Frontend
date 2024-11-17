fe09d9c0ce5a1de404dbc85a8e4bd3a78a375998
Jay200123 <kenken.ayonon@gmail.com>
2024-11-17 11:34:43 +0800

    Increase footer height for improved visibility

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function () {
   return (
     <>
-      <footer className='w-full h-20 bg-gray-800'>
+      <footer className='w-full h-32 bg-gray-800'>
         <h3>hi this is a footer</h3>
       </footer>
     </>

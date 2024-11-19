7ee81ef2e8ddec60d00c90df1b5f4137ad571f4f
Jay200123 <kenken.ayonon@gmail.com>
2024-11-19 15:34:14 +0800

    Refactor import statements for consistency and readability

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,15 +1,15 @@
-import Home from './Home';
-import Login from './Login';
-import AuthenticatedTest from './AuthenticatedTest';
-import Unauthorized from './Unauthorized';
-import SignUp from './SignUp';
-import Dashboard from './Dashboard';
+import Home from './Home'
+import Login from './Login'
+import AuthenticatedTest from './AuthenticatedTest'
+import Unauthorized from './Unauthorized'
+import SignUp from './SignUp'
+import Dashboard from './Dashboard'
 
 export {
-    Home,
-    Login,
-    AuthenticatedTest,
-    Unauthorized,
-    SignUp,
-    Dashboard   
+  Home,
+  Login,
+  AuthenticatedTest,
+  Unauthorized,
+  SignUp,
+  Dashboard,
 }

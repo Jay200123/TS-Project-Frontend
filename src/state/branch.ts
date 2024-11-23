39bb0454ea9f48636ec70ac14635f700384815a9
Jay200123 <kenken.ayonon@gmail.com>
2024-11-23 17:45:34 +0800

    Add branch management store with CRUD operations

---
+++ src/state/branch.ts
@@ -0,0 +1,72 @@
+import { create } from "zustand";
+import axios from "axios";
+import { BranchState } from "../interface";
+
+export const useBranchStore = create<BranchState>((set) => ({
+    branches: [],
+    branch: null,
+    loading: false,
+    error: null,
+    getAllBranches: async () => {
+        const res = await axios.get(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/branches`);
+        set({ branches: res.data.details, loading: false, error: null });
+
+        return res.data.details;    
+    },
+
+    getOneBranch: async (id) => {
+        const res = await axios.get(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/branches/${id}`);
+        set({
+            branch: !Array.isArray(res.data.details) ? res.data.details : null,
+            loading: false,
+            error: null,
+        });
+
+        return res.data.details;
+    },
+    
+    createBranch: async (formData) => {
+        const res = await axios.post(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/branches`,
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            }
+        );
+        set((state) => ({
+            branches: [...state.branches, res.data.details],
+            loading: false,
+            error: null
+        }));
+
+        return  res.data.details;
+    },
+
+    updateBranchById: async (id, formData) => {
+        const res = await axios.patch(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/branches/edit/${id}`,
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            }
+        )
+
+        set((state) => ({
+            branches: state.branches.map((u) =>
+                u._id === id ? res.data.details : u
+            ),
+        }));
+
+        return res.data.details;    
+
+    },
+    deleteBranchById: async (id) => {
+        await axios.delete(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/branches/delete/${id}`);
+        set((state) => ({
+            branches: state.branches.filter((u) => u._id !== id),
+        }));
+    }
+
+}))

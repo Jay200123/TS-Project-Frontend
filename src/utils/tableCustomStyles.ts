ea3d261649174f431ddcde487c9a7dc334a2df1b
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 11:33:50 +0800

    Add custom table styles for improved UI consistency

---
+++ src/utils/tableCustomStyles.ts
@@ -0,0 +1,16 @@
+const tableCustomStyles = {
+    headCells: {
+      style: {
+        backgroundColor: "#F2F2F2",
+        fontSize: "14px",
+      },
+    },
+    cells: {
+      style: {
+        backgroundColor: "#F2F2F2",
+        fontSize: "14px",
+      },
+    },
+  };
+  
+  export { tableCustomStyles };

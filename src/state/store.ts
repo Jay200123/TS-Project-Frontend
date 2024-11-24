c99775860b4a64295e35d9d0b87d522c698ea240
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 15:15:37 +0800

    Add position store to state management exports

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,10 +2,12 @@ import useAuthenticationStore from "./authentication";
 import { useUserStore } from "./user";
 import { useBranchStore } from "./branch";
 import { useDepartmentStore } from "./department";
+import { usePositionStore } from "./position";
 
 export {
-    useAuthenticationStore,
-    useUserStore,
-    useBranchStore,
-    useDepartmentStore
-}
+  useAuthenticationStore,
+  useUserStore,
+  useBranchStore,
+  useDepartmentStore,
+  usePositionStore,
+};

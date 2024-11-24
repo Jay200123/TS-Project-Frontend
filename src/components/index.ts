05a6ccc9b039545eb86d237b001e050e67fe7be3
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 11:32:51 +0800

    Add AdminNavbar and EmployeeNavbar components to index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,15 @@
-import Navbar from "./Navbar";
+import Navbar  from "./Navbar";
+import AdminNavbar from "./Navbar/AdminNavbar";
+import EmployeeNavbar from "./Navbar/EmployeeNavbar";
 import Footer from "./Footer";
 import ProtectedRoute from "./ProtectedRoutes";
 import Image from "./Image";
 
 export {
     Navbar,
+    AdminNavbar,
+    EmployeeNavbar,
     Footer,
+    Image,
     ProtectedRoute,
-    Image
 }

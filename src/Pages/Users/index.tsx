ae92eef22c8eeeb7cb0098b4157dda595f3b9e18
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 11:33:45 +0800

    Add UsersTable component for managing user records with delete functionality

---
+++ src/Pages/Users/index.tsx
@@ -0,0 +1,124 @@
+import DataTable, { TableColumn } from 'react-data-table-component'
+import { FaEye, FaTrash } from 'react-icons/fa'
+import { FadeLoader } from 'react-spinners'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useUserStore } from '../../state/store'
+import { useNavigate } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { tableCustomStyles } from '../../utils/tableCustomStyles'
+import { User } from '../../interface'
+
+export default function () {
+  const navigate = useNavigate()
+  const { users, loading, getAllUsers, deleteUserById } = useUserStore()
+
+  useQuery({
+    queryKey: ['users'],
+    queryFn: getAllUsers
+  });
+
+  const handleDelete = async (id: string) => {
+    if (window.confirm('Are you sure you want to delete this User?')) {
+      await deleteUserById(id)
+      toast.success('User Deleted Successfully')
+    }
+  }
+
+  const columns: TableColumn<User>[] = [
+    {
+      name: 'First Name',
+      selector: row => row.fname,
+      sortable: true
+    },
+    {
+      name: 'Last Name',
+      selector: row => row.lname,
+      sortable: true
+    },
+    {
+      name: 'Contact Number',
+      selector: row => row.phone,
+      sortable: true
+    },
+    {
+      name: 'Adress',
+      selector: row => row.address,
+      sortable: true
+    },
+    {
+      name: 'City',
+      selector: row => row.city,
+      sortable: true
+    },
+    {
+      name: 'Email',
+      selector: row => row.email,
+      sortable: true
+    },
+    // {
+    //     name: 'Branch',
+    //     selector: row => row.branch,
+    // }
+    {
+      name: 'Images',
+      cell: row => {
+        const randomImage =
+          row.image?.length > 0
+            ? row.image[Math.floor(Math.random() * row.image?.length)]
+            : null
+
+        return (
+          <div className='grid items-center justify-center'>
+            {randomImage && (
+              <img
+                className='object-center w-10 h-10 rounded-full'
+                src={randomImage.url}
+                alt={randomImage.originalname}
+              />
+            )}
+          </div>
+        )
+      }
+    },
+    {
+      name: 'Actions',
+      cell: row => (
+        <div className='flex items-center text-center'>
+          <FaEye
+            className='mr-2 text-xl text-green-300'
+            onClick={() => navigate(`/admin/user/${row._id}`)}
+          />
+          <FaTrash
+            className='text-xl text-red-500'
+            onClick={() => handleDelete(row._id)}
+          />
+        </div>
+      )
+    }
+  ]
+
+  return (
+    <>
+      {loading ? (
+        <div className='mt-8 loader'>
+          <FadeLoader color='#FFB6C1' loading={true} height={15} width={5} />
+        </div>
+      ) : (
+        <div className="h-full m-4 p-8 bg-white rounded-lg shadow-lg">
+          <DataTable
+            title='Users Records'
+            columns={columns}
+            data={users}
+            pagination
+            highlightOnHover
+            pointerOnHover
+            paginationPerPage={10}
+            paginationRowsPerPageOptions={[10, 20, 30]}
+            customStyles={tableCustomStyles}
+          />
+        </div>
+      )}
+    </>
+  )
+}

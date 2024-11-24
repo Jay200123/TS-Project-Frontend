52c438a81d62dd0f1002d0f7c1e2e3fd43e5524d
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 20:13:47 +0800

    Enhance user state management with activation functionality and code formatting improvements

--- src/state/user.ts
+++ src/state/user.ts
@@ -4,71 +4,89 @@ import axios from "axios";
 import api from "./api";
 
 const useUserStore = create<UserState>((set) => ({
-    users: [],
-    user: null,
-    loading: false,
-    error: null,
-    getAllUsers: async () => {
-        const res = await api.get(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/users`);
-        set({ users: res.data.details, loading: false, error: null });
+  users: [],
+  user: null,
+  loading: false,
+  error: null,
+  getAllUsers: async () => {
+    const res = await api.get(
+      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/users`
+    );
+    set({ users: res.data.details, loading: false, error: null });
 
-        return res.data.details;
-    },
+    return res.data.details;
+  },
 
-    getOneUser: async (id) => {
-        const res = await api.get(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/${id}`);
-        set({
-            user: !Array.isArray(res.data.details) ? res.data.details : null,
-            loading: false,
-            error: null,
-        });
-        return res.data.details;
-    },
+  getOneUser: async (id) => {
+    const res = await api.get(
+      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/${id}`
+    );
+    set({
+      user: !Array.isArray(res.data.details) ? res.data.details : null,
+      loading: false,
+      error: null,
+    });
+    return res.data.details;
+  },
 
-    createUser: async (formData) => {
-        const res = await axios.post(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/users`,
-            formData,
-            {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            }
-        );
-        set((state) => ({
-            users: [...state.users, res.data.details],
-            loading: false,
-            error: null
-        }))
-        return res.data.details;
-    },
+  createUser: async (formData) => {
+    const res = await axios.post(
+      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/users`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    set((state) => ({
+      users: [...state.users, res.data.details],
+      loading: false,
+      error: null,
+    }));
+    return res.data.details;
+  },
 
-    updateUserById: async (id, formData) => {
-        const res = await api.patch(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/edit/${id}`,
-            formData,
-            {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            }
-        )
+  updateUserById: async (id, formData) => {
+    const res = await api.patch(
+      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/edit/${id}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
 
-        set((state) => ({
-            users: state.users.map((u) =>
-                u._id === id ? res.data.details : u
-            ),
-        }));
-        return res.data.details;
-    },
+    set((state) => ({
+      users: state.users.map((u) => (u._id === id ? res.data.details : u)),
+    }));
+    return res.data.details;
+  },
 
-    deleteUserById: async (id) => {
-        const res = await api.delete(`${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/${id}`);
-        set((state) => ({
-            users: state.users.filter((u) => u._id !== id),
-            loading: false,
-            error: null,
-        }));
-        return res.data.details;
-    }
+  deleteUserById: async (id) => {
+    await api.delete(
+      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/${id}`
+    );
+    set((state) => ({
+      users: state.users.filter((u) => u._id !== id),
+      loading: false,
+      error: null,
+    }));
+  },
+
+  activateUserById: async (id) => {
+    const res = await api.patch(
+      `${import.meta.env.VITE_URI}${
+        import.meta.env.VITE_API
+      }/user/activate/${id}`
+    );
+
+
+    set((state) => ({
+      users: state.users.map((u) => (u._id === id ? res.data.details : u)),
+    }));
+  },
 }));
 
-export { useUserStore }
+export { useUserStore };

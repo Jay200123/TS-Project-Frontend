d96a0906200e8affb22edca168cec5e24420eb92
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 15:14:40 +0800

    Add PositionState interface and update Position interface with ID field

--- src/interface/position.ts
+++ src/interface/position.ts
@@ -1,9 +1,22 @@
 import { Department } from "./department";
 
 interface Position {
-    department: Department;
-    position_name: string;
-    description: string;
+  _id: string;
+  department: Department;
+  position_name: string;
+  description: string;
 }
 
-export type { Position };
+interface PositionState {
+  positions: Position[];
+  position: Position | null;
+  loading: boolean;
+  error: string | null;
+  getAllPositions: () => Promise<Position[]>;
+  getOnePosition: (id: string) => Promise<Position | null>;
+  createPosition: (formData: FormData) => Promise<Position>;
+  updatePositionById: (id: string, formData: FormData) => Promise<Position>;
+  deletePositionById: (id: string) => Promise<Position>;
+}
+
+export type { Position, PositionState };

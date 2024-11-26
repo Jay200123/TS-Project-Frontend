93bde19bb3e8c8d0cee1486021e4141656d6c84e
Jay200123 <kenken.ayonon@gmail.com>
2024-11-26 10:16:48 +0800

    Refactor HomeLayout component for improved structure and layout

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from 'react-router-dom'
-import { Footer, Navbar } from '../components'
+import { Outlet } from 'react-router-dom';
+import { Footer, Navbar } from '../components';
 export default function () {
   return (
     <>
       <main className='flex flex-col items-center justify-between min-h-screen min-w-screen'>
         <Navbar />
-        <div>
-          <Outlet />
+        <div className='w-full h-full'>
+        <Outlet />
         </div>
         <Footer />
       </main>

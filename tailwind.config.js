12b4a504a23d29be00221d1f8a0bb03eab5c91fa
Jay200123 <kenken.ayonon@gmail.com>
2024-11-26 19:57:30 +0800

    Add custom background images and colors to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'test-image': "url('/src/assets/bg-test-2.jpg')",
+        'custom-test-blue': "#e6ffff"
+      }
+    }
   },
   plugins: []
 }

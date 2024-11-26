6ca0997aca790ac836d27404a510c6f62fa7468b
Jay200123 <kenken.ayonon@gmail.com>
2024-11-26 16:11:27 +0800

    Refactor Image component layout to use a div wrapper for improved styling and spacing

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -3,9 +3,9 @@ import ticketImage from "../../assets/picture-1.jpeg";
 export default function(){
     return(
         <>
-        <main>
-            <img className="h-full m-6 w-full object-cover rounded-l-lg" src={ticketImage}/>
-        </main>
+        <div className="m-6 p-2">
+            <img className="h-full w-full object-cover rounded-l-lg" src={ticketImage}/>
+        </div>
         </>
     )
 }

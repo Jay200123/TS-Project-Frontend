7553612deb346348c5917149475e424dbf33077e
Jay200123 <kenken.ayonon@gmail.com>
2024-11-26 19:57:50 +0800

    Add RegisterImage import to index for component accessibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,12 +4,13 @@ import EmployeeNavbar from "./Navbar/EmployeeNavbar";
 import Footer from "./Footer";
 import ProtectedRoute from "./ProtectedRoutes";
 import Image from "./Image";
-
+import RegisterImage from "./Image/RegisterImage";
 export {
     Navbar,
     AdminNavbar,
     EmployeeNavbar,
     Footer,
     Image,
+    RegisterImage,
     ProtectedRoute,
 }

cf7fadd0927d4b5ef72b97d0e0f8aaf9e589ee67
Jay200123 <kenken.ayonon@gmail.com>
2024-11-27 17:18:25 +0800

    Enhance Unauthorized page with back navigation button and improved styling

--- src/Pages/Unauthorized/index.tsx
+++ src/Pages/Unauthorized/index.tsx
@@ -1,7 +1,12 @@
+const back = ()=>{
+    window.history.back();  
+}
+
 export default function(){
     return (
-        <div>
-            <h1>Unauthorized 401</h1>
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-5xl font-bold"> :( Unauthorized 401</h1>
+            <button onClick={back} className="p-4 bg-gray-700 text-white text-lg transition-all duration-500 border rounded-md border-gray-700 hover:bg-white hover:text-black">Go Back</button>
         </div>
     )
 };

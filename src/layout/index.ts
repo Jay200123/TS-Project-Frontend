2e6caf4478fd62b1b94fc6e465d356c694e7d882
Jay200123 <kenken.ayonon@gmail.com>
2024-11-27 17:18:07 +0800

    Add TechnicianLayout to layout exports

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,9 +1,11 @@
 import AdminLayout from "./AdminLayout";
 import CustomerLayout from "./EmployeeLayout";
 import HomeLayout from "./HomeLayout";
+import TechnicianLayout from "./TechnicianLayout";
 
 export {
     AdminLayout,
     CustomerLayout,
-    HomeLayout
+    HomeLayout,
+    TechnicianLayout,
 }

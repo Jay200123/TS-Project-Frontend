d6f027afe031827310eef5243f1dd2cdb1f80984
Jay200123 <kenken.ayonon@gmail.com>
2024-11-28 20:11:07 +0800

    Refactor user store API calls to use constants for improved maintainability

--- src/state/user.ts
+++ src/state/user.ts
@@ -3,6 +3,7 @@ import { UserState } from "../interface";
 import axios from "axios";
 import api from "./api";
 import { multipart } from "./multipart";
+import { PATH } from "../constants";
 
 const useUserStore = create<UserState>((set) => ({
   users: [],
@@ -10,18 +11,14 @@ const useUserStore = create<UserState>((set) => ({
   loading: false,
   error: null,
   getAllUsers: async () => {
-    const res = await api.get(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/users`
-    );
+    const res = await api.get(`${import.meta.env.VITE_API_URI}${PATH.USERS_ROUTE}`);
     set({ users: res.data.details, loading: false, error: null });
 
     return res.data.details;
   },
 
   getOneUser: async (id) => {
-    const res = await api.get(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/${id}`
-    );
+    const res = await api.get(`${import.meta.env.VITE_API_URI}${PATH.USER_ID_ROUTE.replace(":id", id)}`);
     set({
       user: !Array.isArray(res.data.details) ? res.data.details : null,
       loading: false,
@@ -32,63 +29,56 @@ const useUserStore = create<UserState>((set) => ({
 
   createUser: async (formData) => {
     const res = await axios.post(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/users`,
+      `${import.meta.env.VITE_API_URI}${PATH.USERS_ROUTE}`,
       formData,
       {
         headers: multipart
       }
     );
-    set((state) => ({
-      users: [...state.users, res.data.details],
-      loading: false,
-      error: null,
-    }));
+    set({ user: res.data.details, loading: false, error: null });
     return res.data.details;
   },
 
   updateUserById: async (id, formData) => {
     const res = await api.patch(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/edit/${id}`,
+      `${import.meta.env.VITE_API_URI}${PATH.EDIT_USER_ROUTE.replace(":id", id)}`,
       formData,
       {
         headers: multipart
       }
     );
 
-    set((state) => ({
-      users: state.users.map((u) => (u._id === id ? res.data.details : u)),
-    }));
+    set({ user: res.data.details, loading: false, error: null });
     return res.data.details;
   },
 
   deleteUserById: async (id) => {
-    await api.delete(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/${id}`
+    const res = await api.delete(
+      `${import.meta.env.VITE_API_URI}${PATH.USER_ID_ROUTE.replace(":id", id)}`
     );
-    set((state) => ({
-      users: state.users.filter((u) => u._id !== id),
-      loading: false,
-      error: null,
-    }));
+
+    set({ user: res.data.details, loading: false, error: null });
   },
 
   activateUserById: async (id) => {
     const res = await api.patch(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API
-      }/user/activate/${id}`
+      `${import.meta.env.VITE_API_URI}${PATH.ACTIVATE_USER_ROUTE.replace(":id", id)}`,
     );
 
-
-    set((state) => ({
-      users: state.users.map((u) => (u._id === id ? res.data.details : u)),
-    }));
+    set({ user: res.data.details, loading: false, error: null });
   },
 
   userProfile: async (id) => {
     const res = await api.get(
-      `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}/user/profile/${id}`
+      `${import.meta.env.VITE_API_URI}${PATH.USER_PROFILE_ROUTE.replace(":id", id)}`
     );
-    set({ user: res.data.details, loading: false, error: null });
+
+    set({
+      user: res.data.details,
+      loading: false,
+      error: null
+    });
+
     return res.data.details;
   }
 }));

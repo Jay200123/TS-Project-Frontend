8a2ffd6d63bfae8414a7d030543644033fd38028
Jay200123 <kenken.ayonon@gmail.com>
2024-11-28 20:11:13 +0800

    Add utility arrays for device types and statuses

---
+++ src/utils/arrays.ts
@@ -0,0 +1,26 @@
+const type = [
+    'Printer',
+    'System Unit',
+    'Laptop',
+    'Monitor',
+    'Mobile',
+    'AVR',
+    'UPS',
+    'Router',
+    'Switch',
+    'Hub',
+    'Access Point',
+    'Cable'
+  ]
+
+  const status = [
+    'Available',
+    'Used',
+    'Defective',
+    'Repair',
+    'Replacement',
+    'Disposal',
+    'Return'
+  ]
+
+  export { type, status };

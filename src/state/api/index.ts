eaebe88a4acef9480ac77691e7b93f907aaa3541
Jay200123 <kenken.ayonon@gmail.com>
2024-11-29 13:49:17 +0800

    Update API base URL configuration for improved clarity and consistency

--- src/state/api/index.ts
+++ src/state/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_URI}${import.meta.env.VITE_API}`,
+  baseURL: `${import.meta.env.VITE_API_URI}`,
 });
 
 api.interceptors.request.use((config) => {

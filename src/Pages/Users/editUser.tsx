2b8237d7c48a9c8c6b2df9605c31b22ab25d3419
Jay200123 <kenken.ayonon@gmail.com>
2024-11-29 17:07:00 +0800

    Add branch, department, and position selection to user edit form

--- src/Pages/Users/editUser.tsx
+++ src/Pages/Users/editUser.tsx
@@ -1,15 +1,22 @@
-import { useUserStore } from '../../state/store'
+import {
+  useUserStore,
+  useBranchStore,
+  useDepartmentStore,
+  usePositionStore
+} from '../../state/store'
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useFormik } from 'formik'
-import { Image } from '../../components'
 import { toast } from 'react-toastify'
 
 export default function () {
   const navigate = useNavigate()
   const { id } = useParams<{ id: string }>()
   const { user, getOneUser, updateUserById } = useUserStore()
+  const { branches, getAllBranches } = useBranchStore()
+  const { departments, getAllDepartments } = useDepartmentStore()
+  const { positions, getAllPositions } = usePositionStore()
 
   useQuery({
     queryKey: ['user', id],
@@ -17,9 +24,27 @@ export default function () {
     enabled: !!id
   })
 
+  useQuery({
+    queryKey: ['branches'],
+    queryFn: getAllBranches
+  })
+
+  useQuery({
+    queryKey: ['departments'],
+    queryFn: getAllDepartments
+  })
+
+  useQuery({
+    queryKey: ['positions'],
+    queryFn: getAllPositions
+  })
+
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
+      branch: user?.branch || '',
+      department: user?.department || '',
+      position: user?.position || '',
       fname: user?.fname || '',
       lname: user?.lname || '',
       phone: user?.phone || '',
@@ -30,6 +55,9 @@ export default function () {
     },
     onSubmit: async values => {
       const formData = new FormData()
+      formData.append('branch', values?.branch.toString())
+      formData.append('department', values?.toString())
+      formData.append('department', values?.toString())
       formData.append('fname', values?.fname)
       formData.append('lname', values?.lname)
       formData.append('phone', values?.phone)
@@ -63,19 +91,130 @@ export default function () {
     }
   })
 
+  const filteredDepartments = departments?.filter(
+    d => d.branch._id === formik.values.branch
+  )
+
+  const filteredPositions = positions?.filter(
+    p => p.department._id === formik.values.department
+  )
+
+  const randomImage =
+    Array.isArray(user?.image) && user.image.length > 0
+      ? user.image[Math.floor(Math.random() * user.image.length)]
+      : null
+
+      const back = () => {  
+        window.history.back()
+      }
+
   return (
     <form
       onSubmit={formik.handleSubmit}
       className='flex items-center justify-center p-4 m-4'
     >
-      <div className='flex flex-col w-full max-w-5xl p-6 space-y-6 bg-white border border-gray-400 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-6'>
+      <div className='relative flex flex-col w-full max-w-5xl p-6 space-y-6 bg-white border border-gray-400 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-6'>
+        <h3 onClick={back} className='top-1 left-1 absolute text-3xl m-1 cursor-pointer transition-all duration-500 hover:text-gray-700'><i className="fa-solid fa-arrow-left"></i></h3>
         <div className='hidden w-full md:w-1/2 md:block mr-12'>
-          <Image />
+          <div className='flex flex-col items-center justify-center'>
+            <h3 className='text-center text-3xl mb-2 font-bold'>User Profile</h3>
+            <img
+              className='object-cover border border-black w-[280px] h-[280px] m-1 rounded-l-lg'
+              src={randomImage?.url}
+              alt={randomImage?.originalname}
+            />
+            <h3 className='text-2xl font-bold mt-1'>
+              {user?.fname} <span> {user?.lname}</span>
+            </h3>
+          </div>
         </div>
         <div className='flex flex-col w-full space-y-4 md:w-1/2'>
           <h2 className='text-2xl font-bold text-center text-gray-800 md:text-left'>
             Edit & Update Information
           </h2>
+
+          <div className='flex flex-col'>
+            <label className='mb-1 text-sm font-medium text-gray-700'>
+              <i className='fa-solid fa-code-branch'></i> Branch
+            </label>
+            <select
+              name='branch'
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.branch.toString()}
+              className='p-2 text-lg border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-full min-h-[2.5rem]'
+            >
+              <option value='' disabled>
+                Select a branch
+              </option>
+              {branches?.map(b => (
+                <option key={b._id} value={b._id}>
+                  {b.branch_name}
+                </option>
+              ))}
+            </select>
+            <div className='min-h-[1.25rem]'>
+              {formik.touched.branch && formik.errors.branch && (
+                <div className='text-sm text-red-500'>
+                  {formik.errors.branch}
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <label className='mb-1 text-sm font-medium text-gray-700'>
+              <i className='mr-1 fa-solid fa-building'></i> Department
+            </label>
+            <select
+              name='department'
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.department.toString()}
+              className='p-2 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            >
+              <option value='' disabled>
+                Select a Department
+              </option>
+              {filteredDepartments?.map(d => (
+                <option key={d._id} value={d._id}>
+                  {d.department_name}
+                </option>
+              ))}
+            </select>
+            {formik.touched.department && formik.errors.department ? (
+              <div className='text-sm text-red-500'>
+                {formik.errors.department}
+              </div>
+            ) : null}
+          </div>
+
+          <div className='flex flex-col'>
+            <label className='mb-1 text-sm font-medium text-gray-700'>
+              <i className='mr-1 fa-solid fa-building'></i> Employee Position
+            </label>
+            <select
+              name='position'
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.position.toString()}
+              className='p-2 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            >
+              <option value='' disabled>
+                Select Your Position
+              </option>
+              {filteredPositions?.map(p => (
+                <option key={p._id} value={p._id}>
+                  {p.position_name}
+                </option>
+              ))}
+            </select>
+            {formik.touched.position && formik.errors.position ? (
+              <div className='text-sm text-red-500'>
+                {formik.errors.position}
+              </div>
+            ) : null}
+          </div>
 
           <div className='flex flex-col'>
             <label className='mb-1 text-sm font-medium text-gray-700'>

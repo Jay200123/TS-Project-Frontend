98e078f2dcadf6edef6bd3816c93a5f81129fa66
Jay200123 <kenken.ayonon@gmail.com>
2024-11-30 17:00:30 +0800

    Refactor layout and button structure in getPositionById component for improved styling consistency

--- src/Pages/Position/getPositionById.tsx
+++ src/Pages/Position/getPositionById.tsx
@@ -22,7 +22,7 @@ export default function () {
   return (
     <form className="flex items-center justify-center p-4 m-4">
       <div className="flex flex-col w-full max-w-5xl p-6 space-y-6 bg-white border border-gray-400 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-6">
-        <div className="hidden w-full md:w-1/2 md:block mr-12">
+        <div className="hidden w-full mr-12 md:w-1/2 md:block">
         <Image/>
         </div>
         <div className="flex flex-col w-full space-y-4 md:w-1/2">
@@ -82,7 +82,7 @@ export default function () {
           </div>
           <div className="flex justify-center mt-4">
             <button onClick={back} className="w-full px-4 py-2 text-lg font-medium text-white transition duration-700 bg-black border border-gray-500 rounded-md hover:opacity-80">
-            <i className="fa-solid fa-arrow-left mr-1"></i>Go Back
+            <i className="mr-1 fa-solid fa-arrow-left"></i>Go Back
             </button>
           </div>
         </div>

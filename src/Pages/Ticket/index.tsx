6f16961aa98bb4dfddef777e43660290c9c9687f
Jay200123 <kenken.ayonon@gmail.com>
2024-11-30 17:00:38 +0800

    Refactor Ticket index component to integrate DataTable for displaying ticket records and streamline ticket fetching logic

--- src/Pages/Ticket/index.tsx
+++ src/Pages/Ticket/index.tsx
@@ -1,156 +1,112 @@
-import { useAuthenticationStore, useDeviceStore } from '../../state/store'
-import { useNavigate } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import { useFormik } from 'formik'
-import { category } from '../../utils/arrays'
+import { useTicketStore } from "../../state/store";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import DataTable, { TableColumn } from "react-data-table-component";
+import { tableCustomStyles } from "../../utils/tableCustomStyles";
+import { Ticket } from "../../interface";
+import FadeLoader from "react-spinners/FadeLoader"; 
 
 export default function () {
-  const navigate = useNavigate()
-  const { user: auth } = useAuthenticationStore()
-  const { devices, getOneDevice, getAllDevices } = useDeviceStore()
+  const navigate = useNavigate();
+  const { tickets, loading, getAllTickets } = useTicketStore();
 
   useQuery({
-    queryKey: ['devices'],
-    queryFn: getAllDevices
-  })
+    queryKey: ["tickets"],
+    queryFn: getAllTickets,
+  });
 
-  const formik = useFormik({
-    initialValues: {
-      device: '',
-      category: '',
-      description: '',
-      image: []
+  const columns: TableColumn<Ticket>[] = [
+    {
+        name:"Ticket ID",
+        selector: row=> row?._id, 
+        sortable: true
     },
-    onSubmit: async values => {
-      const formData = new FormData()
-      formData.append('device', values.device)
-      formData.append('category', values.category)
-      formData.append('description', values.description)
-      values.image.forEach(file => {
-        formData.append('image', file)
-      })
-
-      // await createTicket(formData);
-      // navigate('/tickets');
-    }
-  })
-
-  const { data: device } = useQuery({
-    queryKey: ['device', formik.values.device],
-    queryFn: () => getOneDevice(formik.values.device),
-    enabled: !!formik.values.device
-  })
-
-  const filteredDevice = devices?.filter(
-    device => device.owner._id === auth?._id
-  )
+    {
+        name: "User",
+        selector: row => `${row?.device?.owner?.fname} ${row?.device?.owner?.lname}`,
+        sortable: true,
+    },
+    {
+        name:"Branch",
+        selector: row=> row?.device?.owner?.branch?.branch_name,
+        sortable: true    
+    },
+    {
+        name: "Department",
+        selector: row => row?.device?.owner?.department?.department_name, 
+        sortable: true  
+    },
+    {
+        name: "Position",
+        selector: row => row?.device?.owner?.position?.position_name,   
+        sortable: true
+    },
+    {
+        name:"Device",
+        selector: row=> row?.device?.type,  
+        sortable: true
+    },
+    {
+      name: "Category",
+      selector: row => row?.category,
+      sortable: true
+    },
+    {
+      name: "Level",
+      selector: row => row?.level,
+      sortable: true
+    },
+    {
+      name:"Issue",
+      selector: row=> row?.description,
+      sortable: true
+    },
+    {
+      name:"Status",
+      selector: row=> row?.status,
+      sortable: true
+    },
+    {
+      name: 'Date Submitted',  
+      selector: row => new Date(row.date_submitted.toLocaleString()).toISOString().split('T')[0],
+      sortable: true  
+    },
+    {
+      name: 'Date Resolved', 
+      selector: row => row?.date_submitted ? new Date(row.date_submitted.toLocaleString()).toISOString().split('T')[0] : "Not Resolved Yet",  
+      sortable: true  
+    },
+    { 
+      name:"Technician",
+      selector: row => row?.assignee ? `${row?.assignee?.fname} ${row?.assignee?.lname}` : "Not Assigned",  
+      sortable: true  
+    },
+  ];
 
   return (
-    <form className='flex items-center justify-center p-4 m-4'>
-      <div className='relative flex flex-col w-full max-w-5xl p-6 space-y-6 bg-white border border-gray-400 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-6'>
-        <h2 className='absolute md:text-3xl font-bold text-center text-gray-800 md:text-left text-xl'>
-          Submit a Ticket
-        </h2>
-        <div className='hidden w-full md:w-1/2 md:block mr-12'>
-          <div className='flex flex-col h-full items-center justify-center'>
-            <h3 className='text-xl font-bold mt-1 underline'>Device Image</h3>
-            <img
-              className='h-60 w-60 object-cover border mt-4 border-gray-600 rounded-md shadow-lg p-2'
-              src={device?.image?.[0]?.url || 'default-placeholder.jpg'}
-              alt={device?.image?.[0]?.originalname || 'Select a device first'}
+    <>
+      {loading ? (
+        <div className="mt-8 loader">
+          <FadeLoader color="#FFB6C1" loading={true} height={15} width={5} />
+        </div>
+      ) : (
+        <div>
+          <div className="max-w-full p-4 overflow-hidden bg-transparent rounded-lg sm:p-6 lg:p-8 sm:max-w-6xl">
+            <DataTable
+              title="Ticket Records"
+              columns={columns}
+              data={tickets}
+              pagination
+              highlightOnHover
+              pointerOnHover
+              paginationPerPage={10}
+              paginationRowsPerPageOptions={[10, 20, 30]}
+              customStyles={tableCustomStyles}
             />
-            <h3 className='font-bold text-lg mt-2'>Device Description</h3>
-            <p className='text-sm mt-1 p-2'>
-              {device?.description
-                ? device?.description
-                : 'Select a device first'}
-            </p>
           </div>
         </div>
-
-        <div className='flex flex-col w-full space-y-4 md:w-1/2'>
-          <div className='flex flex-col'>
-            <label className='mb-1 text-sm font-medium text-gray-700'>
-              <i className='fa-solid fa-computer'></i> Select a Device
-            </label>
-            <select
-              name='device'
-              id='device'
-              onChange={formik.handleChange}
-              value={formik.values.device}
-              className='w-full text-base border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 min-h-[2.5rem]'
-            >
-              <option value='' disabled>
-                Select a Device
-              </option>
-              {filteredDevice?.map(d => (
-                <option key={d?._id} value={d?._id}>
-                  {d?.type}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className='flex flex-col'>
-            <label className='mb-1 text-sm font-medium text-gray-700'>
-              <i className='fa-solid fa-list'></i> Category
-            </label>
-            <select
-              name='category'
-              id='category'
-              onChange={formik.handleChange}
-              value={formik.values.category}
-              className='w-full text-base border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 min-h-[2.5rem]'
-            >
-              <option value='' disabled>
-                Category
-              </option>
-              {category?.map((c, index) => (
-                <option key={index} value={c}>
-                  {c}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className='flex flex-col'>
-            <label className='mb-1 text-sm font-medium text-gray-700'>
-              <i className='mr-1 fa-solid fa-pencil'></i> Concern
-            </label>
-            <textarea
-              id='description'
-              name='description'
-              onChange={formik.handleChange}
-              value={formik.values.description}
-              className='h-[125px] w-full p-2 placeholder-black border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-            />
-          </div>
-
-          <div className='flex flex-col'>
-            <label className='mb-1 text-sm font-medium text-gray-700'>
-              <i className='mr-1 fa-solid fa-image'></i> Submit a Picture of the
-              Issue
-            </label>
-            <input
-              type='file'
-              name='image'
-              multiple
-              onChange={e => {
-                const files = Array.from(e.currentTarget.files || [])
-                formik.setFieldValue('image', files)
-              }}
-            />
-          </div>
-          <div className='flex justify-center mt-4'>
-            <button
-              type='submit'
-              className='w-full px-4 py-2 text-lg font-medium text-white transition duration-700 bg-black border border-gray-500 rounded-md hover:opacity-80'
-            >
-              Create Ticket
-            </button>
-          </div>
-        </div>
-      </div>
-    </form>
-  )
+      )}
+    </>
+  );
 }

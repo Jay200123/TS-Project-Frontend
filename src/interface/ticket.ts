c989931931fc98a568e1ef2b8eb8595d117491c4
Jay200123 <kenken.ayonon@gmail.com>
2024-11-30 16:59:58 +0800

    Refactor Ticket and TicketState interfaces for improved type definitions and method signatures

--- src/interface/ticket.ts
+++ src/interface/ticket.ts
@@ -2,34 +2,33 @@ import { Device } from "./device";
 import { Image } from "./image";
 import { User } from "./user";
 
-type Status = "pending"| "resolved" | "in-progress" | "closed";
+type Status = "pending" | "resolved" | "in-progress" | "closed";
 type Category = "hardware" | "software" | "network";
 type Level = "urgent" | "priority" | "non-urgent";
 
-
 interface Ticket {
-    device: Device;
-    description: string;    
-    date_submitted: Date;
-    date_resolved: Date;    
-    status: Status;
-    category: Category; 
-    level: Level;
-    assignee: User;
-    image: Image[];
-
+  _id: string;
+  device: Device;
+  description: string;
+  date_submitted: Date;
+  date_resolved: Date;
+  status: Status;
+  category: Category;
+  level: Level;
+  assignee: User;
+  image: Image[];
 }
 
 interface TicketState {
-    tickets: Ticket[];
-    ticket: Ticket;
-    loading: boolean;
-    error: string;
-    getAllTickets: () => void;  
-    getTicket: (id: string) => void;
-    createTicket: (ticket: Ticket) => void;
-    updateTicket: (ticket: Ticket) => void;
-    deleteTicket: (id: string) => void;
-    
+  tickets: Ticket[];
+  ticket: Ticket | null;
+  loading: boolean;
+  error: string;
+  getAllTickets: () => Promise<Ticket>;
+  getOneTicket: (id: string) => Promise<Ticket>;
+  createTicket: (formData: FormData) => Promise<void>;
+  updateTicketById: (ticket: string, formData: FormData) => Promise<void>;
+  deleteTicketById: (id: string) => Promise<void>;
 }
 
+export type { Ticket, TicketState };

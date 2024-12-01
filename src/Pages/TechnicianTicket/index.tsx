1e50762813ba109ff60b3223f651fd773f232cac
Jay200123 <kenken.ayonon@gmail.com>
2024-12-01 21:14:50 +0800

    Add TechnicianTicket component for displaying technician-specific tickets

---
+++ src/Pages/TechnicianTicket/index.tsx
@@ -0,0 +1,137 @@
+import { useQuery } from '@tanstack/react-query'
+import { useAuthenticationStore, useTicketStore } from '../../state/store'
+
+export default function () {
+  const { user: auth } = useAuthenticationStore()
+  const { tickets, getAllTickets } = useTicketStore()
+
+  useQuery({
+    queryKey: ['tickets'],
+    queryFn: getAllTickets
+  })
+
+  const filteredTickets = tickets.filter(t => t.assignee?._id === auth?._id)
+
+  if (tickets.length === 0) {
+    ;<h3 className='text-center text-sm md:text-[18px]'>No Devices Found</h3>
+  }
+
+  return (
+    <div className='p-7'>
+      <h3 className='text-sm md:text-2xl font-bold'>My Tickets</h3>
+      {filteredTickets.map(t => (
+        <div key={t?._id} className='relative flex flex-col justify-evenly'>
+          <div className='flex-col flex md:flex-row items-center justify-start p-2 m-4 sm:h-[300px] md:h-[450px] overflow-hidden rounded-md shadow-lg border border-gray'>
+            <div className='flex flex-col items-center justify-center w-1/4 p-2 m-2'>
+              <h3 className='text-xs md:text-lg font-bold text-center'>
+                Ticket Image
+              </h3>
+              {t?.image?.length > 1 ? (
+                <img
+                  className='w-[130px] h-[130px] md:w-[180px] md:h-[180px] shadow-md'
+                  src={
+                    t?.image[Math.floor(Math.random() * t?.image.length)]?.url
+                  }
+                  alt='test image'
+                />
+              ) : (
+                <img
+                  className='rounded-sm md:w-52 md:h-52'
+                  src={t?.image[0]?.url || ''}
+                  alt='image'
+                />
+              )}
+              <button className='bg-green-500 p-3 rounded-md text-sm text-white md:text-lg transition-all duration-500 hover:bg-green-800'>
+                More Details
+              </button>
+            </div>
+            <div className='w-9/12 p-1'>
+              <div className='flex items-center justify-between'>
+                <h1 className='font-bold text-[14px] md:text-[18px] m-1'>
+                  Type:{' '}
+                  <span className='text-[14px] md:text-[18px] font-medium'>
+                    {t?.device?.type}
+                  </span>
+                </h1>
+                <h1
+                  className={`font-bold text-[14px] md:text-[18px] m-1 ${
+                    t?.status === 'pending' ? 'text-green-500' : 'text-black'
+                  }`}
+                >
+                  {t?.status}
+                </h1>
+              </div>
+              <h3 className='font-bold text-[14px] md:text-[17px] m-1'>
+                Description:
+              </h3>
+              <p className='text-[12px] md:text-[16px] m-1 p-1'>
+                {t?.device?.description}
+              </p>
+              <h3 className='font-bold text-[12px] md:text-[16px] mb-1'>
+                Serial Number:{' '}
+                <span className='font-medium underline'>
+                  {t?.device?.serial_number}
+                </span>
+              </h3>
+              <h3 className='font-bold text-[12px] md:text-[16px] mb-1'>
+                {t?.category[0]?.toUpperCase() + t?.category.slice(1)} Issue
+              </h3>
+              <h3 className='font-bold text-[12px] md:text-[16px] mb-1'>
+                {t?.level[0]?.toUpperCase() + t?.level.slice(1)}
+              </h3>
+              <h3 className='font-bold text-[12px] md:text-[17px] m-1'>
+                Concern:
+              </h3>
+              <textarea
+                readOnly
+                className='placeholder:text-black w-full h-20 p-1 text-[12px] md:text-[16px] border border-gray rounded-md'
+                placeholder={t?.description}
+              />
+              <div className='flex items-center justify-between'>
+                <p className='font-bold text-[12px] md:text-[17px]'>
+                  Date Submitted:
+                  <span className='underline font-medium text-[10px] md:text-[16px] m-1'>
+                    {
+                      new Date(t?.date_submitted.toLocaleString())
+                        .toISOString()
+                        .split('T')[0]
+                    }
+                  </span>
+                </p>
+                <p className='font-bold text-[12px] md:text-[17px]'>
+                  Date Resolved:
+                  <span
+                    className={`underline font-medium text-[10px] md:text-[16px] m-1 ${
+                      t?.date_resolved ? `text-black` : `text-red-700`
+                    }`}
+                  >
+                    {t?.date_resolved
+                      ? new Date(t?.date_resolved.toLocaleString())
+                          .toISOString()
+                          .split('T')[0]
+                      : 'Not Resolved'}
+                  </span>
+                </p>
+              </div>
+              <div className='flex items-center justify-between mt-2'>
+                <p className='font-bold text-[12px] md:text-[17px]'>
+                  Submitted By:
+                  <span className='font-medium text-[10px] md:text-[16px]'>
+                    {t?.device?.owner?.fname} {t?.device?.owner?.lname}
+                  </span>{' '}
+                  from{' '}
+                  <span className='font-medium text-[10px] md:text-[16px] m-1'>
+                    {t?.device?.owner?.department?.department_name}
+                  </span>
+                </p>
+                <button className='p-2 bg-gray-700 text-white text-sm md:text-lg rounded-md border border-gray-700 transition-all duration-500 hover:bg-white hover:text-black'>
+                  Update Ticket
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}

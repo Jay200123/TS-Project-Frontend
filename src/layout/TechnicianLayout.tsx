029898749f21a533299c8eaa1dc69a8ef8899e58
Jay200123 <kenken.ayonon@gmail.com>
2024-12-01 21:14:26 +0800

    Replace Navbar with TechnicianNavbar in TechnicianLayout for technician-specific navigation

--- src/layout/TechnicianLayout.tsx
+++ src/layout/TechnicianLayout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { Footer, Navbar } from '../components';
+import { Footer, TechnicianNavbar } from '../components';
+
 export default function () {
   return (
     <>
       <main className='flex flex-col items-center justify-between min-h-screen min-w-screen'>
-        <Navbar />
+        <TechnicianNavbar />
         <div className='max-w-full max-h-full'>
         <Outlet />
         </div>

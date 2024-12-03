793f7b54399e82070ade6f429f47123ee0025dc2
Jay200123 <kenken.ayonon@gmail.com>
2024-12-03 19:49:50 +0800

    Add history state management with Zustand for API interactions

---
+++ src/state/history.ts
@@ -0,0 +1,28 @@
+import api from "./api";
+import { create } from "zustand";
+import { HistoryState } from "../interface/history";
+import { PATH } from "../constants";
+
+export const useHistoryStore = create<HistoryState>((set) => ({
+    histories: [],
+    history: null,
+    error: "",
+    loading: false,
+    getAllHistories: async () => {
+        const res = await api.get(`${import.meta.env.VITE_API_URI}${PATH.HISTORIES_ROUTE}`);
+        set({ histories: res.data.details, loading: false, error: "" });
+
+        return res?.data?.details;
+    },
+
+    getOneHistory: async (id: string) => {
+        const res = await api.get(`${import.meta.env.VITE_API_URI}${PATH.HISTORY_ID_ROUTE.replace(":id", id)}`);
+        set({ history: !Array.isArray(res.data.details) ? res.data.details : null, loading: false, error: "" });
+
+        return res?.data?.details;
+    },
+    deleteHistoryById: async (id: string) => {
+        const res = await api.delete(`${import.meta.env.VITE_API_URI}${PATH.HISTORY_ID_ROUTE.replace(":id", id)}`);
+        set({ history: res.data.details, loading: false, error: "" });
+    }
+}));

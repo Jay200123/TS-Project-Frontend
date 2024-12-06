e3c5afc30e52b261585a80af312334d55da6c5cb
Jay200123 <kenken.ayonon@gmail.com>
2024-12-06 20:00:40 +0800

    Add HistoryTable import to index file

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,43 +1,44 @@
-import Home from "./Home";
-import Login from "./Login";
-import AuthenticatedTest from "./AuthenticatedTest";
-import Unauthorized from "./Unauthorized";
-import SignUp from "./SignUp";
-import Dashboard from "./Dashboard";
-import UsersTable from "./Users";
-import ApproveUserTable from "./Users/approveUser";
-import GetUserById from "./Users/getUserById";
-import AdminProfile from "./Profile/AdminProfile";
-import EditUser from "./Users/editUser";
-import DepartmentTable from "./Department";
-import CreateDepartment from "./Department/createDepartment";
-import GetDepartmentById from "./Department/getDepartmentById";
-import EditDepartment from "./Department/editDepartment";
-import PositionTable from "./Position";
-import CreatePosition from "./Position/createPosition";
-import GetPositionById from "./Position/getPositionById";
-import EditPosition from "./Position/editPosition";
-import GetAllBranch from "./Branch";
-import CreateBranch from "./Branch/createBranch";
-import GetBranchById from "./Branch/getBranchById";
-import EditBranch from "./Branch/editBranch";
-import EmployeeProfile from "./Profile/EmployeeProfile";
-import TechnicialProfile from "./Profile/TechnicialProfile";
-import TechnicianSignup from "./SignUp/TechnicianSignup";
-import SignUpChoice from "./SignUp/SignUpChoice";
-import DeviceTable from "./Device";
-import GetDeviceById from "./Device/getDeviceById";
-import CreateDevice from "./Device/createDevice";
-import EditDevice from "./Device/editDevice";
-import UserDevices from "./UserDevices";
-import TicketForm from "./CreateTicket";  
-import CreateDeviceByUsers from "./Device/createDeviceByEmployee";
-import TicketTable from "./Ticket";
-import AssignTicket from "./Ticket/assignTicket";
-import TechnicianTickets from "./TechnicianTicket";
-import GetTicketById from "./Ticket/getTicketById";
-import EditTicketByTechnician from "./Ticket/editTicketByTechnician";
-import EmployeeTicket from "./EmployeeTicket";
+import Home from './Home'
+import Login from './Login'
+import AuthenticatedTest from './AuthenticatedTest'
+import Unauthorized from './Unauthorized'
+import SignUp from './SignUp'
+import Dashboard from './Dashboard'
+import UsersTable from './Users'
+import ApproveUserTable from './Users/approveUser'
+import GetUserById from './Users/getUserById'
+import AdminProfile from './Profile/AdminProfile'
+import EditUser from './Users/editUser'
+import DepartmentTable from './Department'
+import CreateDepartment from './Department/createDepartment'
+import GetDepartmentById from './Department/getDepartmentById'
+import EditDepartment from './Department/editDepartment'
+import PositionTable from './Position'
+import CreatePosition from './Position/createPosition'
+import GetPositionById from './Position/getPositionById'
+import EditPosition from './Position/editPosition'
+import GetAllBranch from './Branch'
+import CreateBranch from './Branch/createBranch'
+import GetBranchById from './Branch/getBranchById'
+import EditBranch from './Branch/editBranch'
+import EmployeeProfile from './Profile/EmployeeProfile'
+import TechnicialProfile from './Profile/TechnicialProfile'
+import TechnicianSignup from './SignUp/TechnicianSignup'
+import SignUpChoice from './SignUp/SignUpChoice'
+import DeviceTable from './Device'
+import GetDeviceById from './Device/getDeviceById'
+import CreateDevice from './Device/createDevice'
+import EditDevice from './Device/editDevice'
+import UserDevices from './UserDevices'
+import TicketForm from './CreateTicket'
+import CreateDeviceByUsers from './Device/createDeviceByEmployee'
+import TicketTable from './Ticket'
+import AssignTicket from './Ticket/assignTicket'
+import TechnicianTickets from './TechnicianTicket'
+import GetTicketById from './Ticket/getTicketById'
+import EditTicketByTechnician from './Ticket/editTicketByTechnician'
+import EmployeeTicket from './EmployeeTicket'
+import HistoryTable from './History'
 
 export {
   Home,
@@ -79,5 +80,6 @@ export {
   TechnicianTickets,
   GetTicketById,
   EditTicketByTechnician,
-  EmployeeTicket
-};
+  EmployeeTicket,
+  HistoryTable
+}

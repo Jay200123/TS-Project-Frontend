fe15299abd344be70d4c94fc802b92ce82a3af50
Jay200123 <kenken.ayonon@gmail.com>
2024-12-07 23:01:42 +0800

    Add charts to Dashboard for device and ticket visualization

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,7 +1,23 @@
+import DeviceCountChart from "./DeviceCountChart";
+import DeviceTypeChart from "./DeviceTypeChart";
+import TicketPieChart from "./TicketPieStatus";
+import CategoryPieChart from "./TicketCategoryChart";
+import TicketLevelChart from "./TicketLevelChart";
+
 export default function () {
   return (
-    <>
-      <h3> this is admin dashboard!</h3>
-    </>
-  )
+    <div className="flex flex-col items-center justify-between w-full h-full p-4">
+      <div className="flex flex-col items-center justify-between w-full p-4 md:flex-row">
+        <DeviceCountChart />
+        <DeviceTypeChart />
+      </div>
+      <div className="flex flex-col items-center justify-between w-full p-4 md:flex-row">
+        <TicketPieChart />
+        <CategoryPieChart />
+      </div>
+      <div className="flex flex-col items-center justify-center w-full p-4 md:flex-row">
+        <TicketLevelChart />
+      </div>
+    </div>
+  );
 }

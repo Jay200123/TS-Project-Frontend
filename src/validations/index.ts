6db57d2d2d798663b8cf9637dcc1394df11f948b
Jay200123 <kenken.ayonon@gmail.com>
2024-12-08 11:36:25 +0800

    Add validation schemas for editing branches, departments, devices, and tickets

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,7 +1,31 @@
 import authenticationValidationSchema from "./authValidators";
 import userValidationSchema from "./userValidators";
+import editUserValidationSchema from "./editUserValidationSchema";
+import createBranchValidationSchema from "./createBranchValidationSchema";
+import editBranchValidationSchema from "./editBranchValidationSchema";
+import createDepartmentValidationSchema from "./createDepartmentValidationSchema";
+import editDepartmentValidationSchema from "./editDepartmentValidationSchema";
+import createPositionValidationSchema from "./createPositionValidationSchema";
+import editPositionValidationSchema from "./editPositionValidationSchema";
+import createDeviceValidationSchema from "./createDeviceValidationSchema";
+import editDeviceValidationSchema from "./editDeviceValidationSchema";
+import createTicketValidationSchema from "./createPositionValidationSchema";
+import editTicketByTechnicianValidationSchema from "../Pages/Ticket/editTicketByTechnician";
+import editAssigneeValidationSchema from "./editTicketAssigneeValidationSchema";
 
 export {
     authenticationValidationSchema,
-    userValidationSchema
+    userValidationSchema,
+    editUserValidationSchema,
+    createBranchValidationSchema,
+    editBranchValidationSchema,
+    createDepartmentValidationSchema,
+    editDepartmentValidationSchema,
+    createPositionValidationSchema,
+    editPositionValidationSchema,
+    createDeviceValidationSchema,
+    editDeviceValidationSchema,
+    createTicketValidationSchema,
+    editTicketByTechnicianValidationSchema,
+    editAssigneeValidationSchema
 }

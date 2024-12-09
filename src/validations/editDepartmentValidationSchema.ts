0b058d73f888573ae048a904603cc879fa693c12
Jay200123 <kenken.ayonon@gmail.com>
2024-12-09 18:44:22 +0800

    Fix typo in edit department validation schema: corrected 'descriptionn' to 'description'

--- src/validations/editDepartmentValidationSchema.ts
+++ src/validations/editDepartmentValidationSchema.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const editDepartmentValidationSchema = yup.object({
     branch: yup.string().required("Branch is required"),
     department_name: yup.string().required("Department Name is required"),
-    descriptionn: yup.string().required("Department description is required"),
+    description: yup.string().required("Department description is required"),
 });
 
 export default editDepartmentValidationSchema;

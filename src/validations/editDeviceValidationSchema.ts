9e0c1da4dc66853b359e8aef56a6055de677c9d3
Jay200123 <kenken.ayonon@gmail.com>
2024-12-09 21:32:58 +0800

    Fix validation schema: correct typo in 'description' field and update 'date_returned' to 'date_purchased'

--- src/validations/editDeviceValidationSchema.ts
+++ src/validations/editDeviceValidationSchema.ts
@@ -3,9 +3,9 @@ import * as yup from "yup";
 const editDeviceValidationSchema = yup.object({
     owner: yup.string().required("Device Owner is required"),
     type: yup.string().required("Type is required"),
-    descriptionn: yup.string().required("Description is required"),
+    description: yup.string().required("Description is required"),
     date_requested: yup.date().required("Date Requested is required"),
-    date_returned: yup.date().required("Date Returned is required"),
+    date_purchased: yup.date().required("Date Purchased is required"),
     serial_number: yup.string().required("Serial Number is required"),
     status: yup.string().required("Status is required"),
 });

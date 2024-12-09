202bfd0d7be6862b9d36459687120904aed9d97e
Jay200123 <kenken.ayonon@gmail.com>
2024-12-09 21:32:53 +0800

    Add user password reset functionality and filter users by name or ID

--- src/Pages/Users/index.tsx
+++ src/Pages/Users/index.tsx
@@ -1,5 +1,5 @@
 import DataTable, { TableColumn } from 'react-data-table-component'
-import { FaEye, FaTrash, FaPenAlt } from 'react-icons/fa'
+import { FaEye, FaTrash, FaPenAlt, FaKey } from 'react-icons/fa'
 import { FadeLoader } from 'react-spinners'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -8,10 +8,12 @@ import { useNavigate } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { tableCustomStyles } from '../../utils/tableCustomStyles'
 import { User } from '../../interface'
+import { useState } from 'react'
 
 export default function () {
   const navigate = useNavigate()
-  const { users, loading, getAllUsers, deleteUserById } = useUserStore()
+  const { users, loading, getAllUsers, resetPassword, deleteUserById } = useUserStore()
+  const[findUser, setFindUser] = useState(''); 
 
   useQuery({
     queryKey: ['users'],
@@ -25,13 +27,26 @@ export default function () {
     }
   }
 
+  const handleReset = async(id: string)=>{
+    if(window.confirm('Are you sure you want to reset this User Password?')){ 
+      await resetPassword(id)
+      toast.success('Password Reset Successfully')
+    }
+  }
+
+  const filteredUsers = users?.filter(u => u?.fullname.includes(findUser) || u?.idnumber.includes(findUser))
+
   const columns: TableColumn<User>[] = [
     {
       name: 'First Name',
       selector: row => row?.fullname,
       sortable: true
     },
-
+    {
+      name: 'ID Number',
+      selector: row => row?.idnumber,
+      sortable: true
+    },
     {
       name: 'Contact Number',
       selector: row => row?.phone,
@@ -71,6 +86,10 @@ export default function () {
             className='mr-2 text-xl text-blue-500'
             onClick={() => navigate(`/user/edit/${row._id}`)}
           />
+          <FaKey
+            className='mr-2 text-xl text-yellow-500'
+            onClick={() => handleReset(row._id)}
+          />
           <FaTrash
             className='text-xl text-red-500'
             onClick={() => handleDelete(row._id)}
@@ -88,18 +107,25 @@ export default function () {
         </div>
       ) : (
         <div className='max-w-full p-4 m-6 overflow-hidden bg-white rounded-lg sm:p-6 lg:p-8 md:w-full'>
-          <div className='flex items-center justify-end m-2'>
+          <div className='flex items-center justify-between m-2'>
             <button
               onClick={() => navigate('/admin/signup')}
               className='text-[16px] bg-gray-700 text-white p-[15px] rounded-md transition-all duration-500  hover:bg-white hover:text-black border border-gray-700'
             >
               Add New User<i className='fa fa-plus ml-[2px]'></i>
             </button>
+            <input
+                type='text'
+                className='w-1/4 p-2 mb-4 border border-gray-400 rounded-lg placeholder:text-black'
+                onChange={e => setFindUser(e.target.value)}
+                placeholder='Find User'
+              />
           </div>
+
           <DataTable
             title='Users Records'
             columns={columns}
-            data={users}
+            data={filteredUsers}
             pagination
             highlightOnHover
             pointerOnHover

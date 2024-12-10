030167e8bf4c9937afc611d1ceffbaf07ec1de62
Jay200123 <kenken.ayonon@gmail.com>
2024-12-10 19:58:47 +0800

    Increase font size in table custom styles for improved readability

--- src/utils/tableCustomStyles.ts
+++ src/utils/tableCustomStyles.ts
@@ -2,14 +2,14 @@ const tableCustomStyles = {
   headCells: {
     style: {
       backgroundColor: "#374151",
-      fontSize: "14px",
+      fontSize: "15px",
       color: "#ffffff",
     },
   },
   cells: {
     style: {
       backgroundColor: "#f2f2f2",
-      fontSize: "14px",
+      fontSize: "15px",
       borderBottom: "1px solid #b3b3b3",
     },
     table: {

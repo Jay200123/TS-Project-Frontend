441072c825c43c7852a54eed1afb4e23db27ded6
Jay200123 <kenken.ayonon@gmail.com>
2024-12-12 21:02:17 +0800

    Remove date resolved requirement from technician ticket validation schema

--- src/validations/editTicketByTechnicianValidationSchema.ts
+++ src/validations/editTicketByTechnicianValidationSchema.ts
@@ -4,7 +4,6 @@ const editTicketByTechnicianValidationSchema = yup.object({
     findings: yup.string().required("Findings is required"),
     device_status: yup.string().required("Device status is required"),  
     status: yup.string().required("Status is required"),
-    date_resolved: yup.string().required("Date resolved is required"),  
 });
 
 export default editTicketByTechnicianValidationSchema;

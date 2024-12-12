0fd547d51db0c3cb6d561ae4793ce638c418f3da
Jay200123 <kenken.ayonon@gmail.com>
2024-12-12 21:02:08 +0800

    Remove password requirement from change password validation schema

--- src/validations/changePasswordValidationSchema.ts
+++ src/validations/changePasswordValidationSchema.ts
@@ -1,7 +1,6 @@
 import * as yup from "yup";
 
 const editPasswordValidationSchema = yup.object({
-    password: yup.string().required("Password is required"),
     newPassword: yup.string().required("New Password is required"),
     confirmPassword: yup.string().required("Confirm Password is required"),
 });

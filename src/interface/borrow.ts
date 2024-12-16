c1e24c238c2f12285782270e60cf96773486ff95
Jay200123 <kenken.ayonon@gmail.com>
2024-12-17 00:37:47 +0800

    Add Borrow and BorrowState interfaces to manage borrowing functionality

---
+++ src/interface/borrow.ts
@@ -0,0 +1,32 @@
+import { User } from "./user";
+import { Equipment } from "./equipment";
+
+interface Borrow {
+  _id: string;
+  user: User;
+  lender: User;
+  equipment: Equipment;
+  serial_number: string;
+  reason: string;
+  borrow_date: Date;
+  return_date: Date;
+  status: string;
+  quantity: number;
+  counter: number;
+  borrowNumber: string;
+  createdAt: Date;
+}
+
+interface BorrowState {
+  borrows: Borrow[];
+  borrow: Borrow | null;
+  loading: boolean;
+  error: string | null;
+  getAllBorrows: () => Promise<Borrow[]>;
+  getOneBorrow: (id: string) => Promise<Borrow | null>;
+  createBorrow: (data: FormData) => Promise<void>;
+  updateBorrowById: (id: string, data: FormData) => Promise<void>;
+  deleteBorrowById: (id: string) => Promise<void>;
+}
+
+export type { Borrow, BorrowState };

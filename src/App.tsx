6211b971bc5d59e36ba5daf375c65c530f2c18e7
Jay200123 <kenken.ayonon@gmail.com>
2024-12-16 16:32:27 +0800

    Add delete functionality for equipment with confirmation prompt

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
+  RouterProvider,
+} from "react-router-dom";
 import {
   Home,
   Login,
@@ -44,29 +44,33 @@ import {
   GetTicketDepartment,
   ChangePassword,
   GetHistoryByDeviceId,
-  GetTechnicianTickets
-} from './Pages'
-import { ProtectedRoute } from './components'
+  GetTechnicianTickets,
+  EquipmentTable,
+  CreateEquipment,
+  GetEquipmentById,
+  EditEquipment,
+} from "./Pages";
+import { ProtectedRoute } from "./components";
 import {
   HomeLayout,
   AdminLayout,
   EmployeeLayout,
-  TechnicianLayout
-} from './layout'
+  TechnicianLayout,
+} from "./layout";
 
-function App () {
+function App() {
   const Router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
         <Route element={<HomeLayout />}>
           {/* Public Routes  */}
           <Route index element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/unauthorized' element={<Unauthorized />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
           <Route
-            path='/change/password'
+            path="/change/password"
             element={
-              <ProtectedRoute userRole={['Employee', 'Technician', 'Admin']}>
+              <ProtectedRoute userRole={["Employee", "Technician", "Admin"]}>
                 <ChangePassword />
               </ProtectedRoute>
             }
@@ -76,49 +80,49 @@ function App () {
         {/* Customer Private Routes */}
         <Route element={<EmployeeLayout />}>
           <Route
-            path='/employee/device'
+            path="/employee/device"
             element={
-              <ProtectedRoute userRole={['Employee']}>
+              <ProtectedRoute userRole={["Employee"]}>
                 <UserDevices />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/ticket/create'
+            path="/ticket/create"
             element={
-              <ProtectedRoute userRole={['Employee']}>
+              <ProtectedRoute userRole={["Employee"]}>
                 <TicketForm />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/employee/ticket/:id'
+            path="/employee/ticket/:id"
             element={
-              <ProtectedRoute userRole={['Employee']}>
+              <ProtectedRoute userRole={["Employee"]}>
                 <GetTicketById />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/employee/tickets'
+            path="/employee/tickets"
             element={
-              <ProtectedRoute userRole={['Employee']}>
+              <ProtectedRoute userRole={["Employee"]}>
                 <EmployeeTicket />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/employee/department/tickets'
+            path="/employee/department/tickets"
             element={
-              <ProtectedRoute userRole={['Employee']}>
+              <ProtectedRoute userRole={["Employee"]}>
                 <GetTicketDepartment />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/employee/history/device/:id'
+            path="/employee/history/device/:id"
             element={
-              <ProtectedRoute userRole={['Employee']}>
+              <ProtectedRoute userRole={["Employee"]}>
                 <GetHistoryByDeviceId />
               </ProtectedRoute>
             }
@@ -128,42 +132,42 @@ function App () {
         {/* Technician Private Routes */}
         <Route element={<TechnicianLayout />}>
           <Route
-            path='/technician/tickets'
+            path="/technician/tickets"
             element={
-              <ProtectedRoute userRole={['Technician']}>
+              <ProtectedRoute userRole={["Technician"]}>
                 <TechnicianTickets />
               </ProtectedRoute>
             }
           />
 
           <Route
-            path='/technician/ticket/:id'
+            path="/technician/ticket/:id"
             element={
-              <ProtectedRoute userRole={['Technician']}>
+              <ProtectedRoute userRole={["Technician"]}>
                 <GetTicketById />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/technician/ticket/edit/:id'
+            path="/technician/ticket/edit/:id"
             element={
-              <ProtectedRoute userRole={['Technician']}>
+              <ProtectedRoute userRole={["Technician"]}>
                 <EditTicketByTechnician />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/technician/all-tickets'
+            path="/technician/all-tickets"
             element={
-              <ProtectedRoute userRole={['Technician']}>
+              <ProtectedRoute userRole={["Technician"]}>
                 <TicketTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/technician/ticket/:id'
+            path="/technician/ticket/:id"
             element={
-              <ProtectedRoute userRole={['Technician']}>
+              <ProtectedRoute userRole={["Technician"]}>
                 <GetTicketById />
               </ProtectedRoute>
             }
@@ -174,95 +178,95 @@ function App () {
         <Route element={<AdminLayout />}>
           {/* SIGN UP ROUTES  */}
           <Route
-            path='/admin/signup'
+            path="/admin/signup"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <SignUpChoice />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/admin/employee/signup'
+            path="/admin/employee/signup"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <SignUp />
               </ProtectedRoute>
             }
           />
 
           <Route
-            path='/admin/technician/signup'
+            path="/admin/technician/signup"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <TechnicianSignup />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/dashboard'
+            path="/dashboard"
             index
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <Dashboard />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/users'
+            path="/users"
             index
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <UsersTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/user/:id'
+            path="/user/:id"
             index
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetUserById />
               </ProtectedRoute>
             }
           />
 
           <Route
-            path='user/edit/:id'
+            path="user/edit/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <EditUser />
               </ProtectedRoute>
             }
           />
           {/* Department Routes */}
           <Route
-            path='/departments'
+            path="/departments"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <DepartmentTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/department/create'
+            path="/department/create"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <CreateDepartment />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/department/:id'
+            path="/department/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetDepartmentById />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/department/edit/:id'
+            path="/department/edit/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <EditDepartment />
               </ProtectedRoute>
             }
@@ -270,33 +274,33 @@ function App () {
 
           {/* Position Routes */}
           <Route
-            path='/positions'
+            path="/positions"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <PositionTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/position/create'
+            path="/position/create"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <CreatePosition />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/position/:id'
+            path="/position/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetPositionById />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/position/edit/:id'
+            path="/position/edit/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <EditPosition />
               </ProtectedRoute>
             }
@@ -304,35 +308,35 @@ function App () {
 
           {/* Branch Routes */}
           <Route
-            path='/branches'
+            path="/branches"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetAllBranch />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/branch/create'
+            path="/branch/create"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <CreateBranch />
               </ProtectedRoute>
             }
           />
 
           <Route
-            path='/branch/:id'
+            path="/branch/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetBranchById />
               </ProtectedRoute>
             }
           />
 
           <Route
-            path='/branch/edit/:id'
+            path="/branch/edit/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <EditBranch />
               </ProtectedRoute>
             }
@@ -340,104 +344,139 @@ function App () {
 
           {/* Device Routes */}
           <Route
-            path='/devices'
+            path="/devices"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <DeviceTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/device/create'
+            path="/device/create"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <CreateDevice />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/device/:id'
+            path="/device/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetDeviceById />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/device/edit/:id'
+            path="/device/edit/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <EditDevice />
               </ProtectedRoute>
             }
           />
+          {/* Ticket Routes  */}
           <Route
-            path='/tickets'
+            path="/tickets"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <TicketTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/ticket/assign/:id'
+            path="/ticket/assign/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <AssignTicket />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/ticket/:id'
+            path="/ticket/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetTicketById />
               </ProtectedRoute>
             }
           />
           {/* History Routes  */}
           <Route
-            path='/histories'
+            path="/histories"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <HistoryTable />
               </ProtectedRoute>
             }
           />
           <Route
-            path='/history/:id'
+            path="/history/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetHistoryById />
               </ProtectedRoute>
             }
           />
-           <Route
-            path='/history/device/:id'
+          <Route
+            path="/history/device/:id"
             element={
-              <ProtectedRoute userRole={['Admin']}>
+              <ProtectedRoute userRole={["Admin"]}>
                 <GetHistoryByDeviceId />
               </ProtectedRoute>
             }
           />
+          {/* Ticket Reports  */}
           <Route
-          path='admin/tickets/reports'
-          element={
-            <ProtectedRoute userRole={['Admin']}>
-              <GetTechnicianTickets/>
-            </ProtectedRoute>
-          }
+            path="admin/tickets/reports"
+            element={
+              <ProtectedRoute userRole={["Admin"]}>
+                <GetTechnicianTickets />
+              </ProtectedRoute>
+            }
+          />
+          {/* Equipment Routes */}
+          <Route
+            path="/equipments"
+            element={
+              <ProtectedRoute userRole={["Admin"]}>
+                <EquipmentTable />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/equipment/create"
+            element={
+              <ProtectedRoute userRole={["Admin"]}>
+                <CreateEquipment />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/equipment/:id"
+            element={
+              <ProtectedRoute userRole={["Admin"]}>
+                <GetEquipmentById />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/equipment/edit/:id"
+            element={
+              <ProtectedRoute userRole={["Admin"]}>
+                <EditEquipment />
+              </ProtectedRoute>
+            }
           />
         </Route>
       </Route>
     )
-  )
+  );
 
   return (
     <>
       <RouterProvider router={Router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

0febab73a5f0bd3809eb4dcfc29b61277571ae5a
Jay200123 <kenken.ayonon@gmail.com>
2024-12-19 22:34:18 +0800

    Add back navigation functionality to getPositionById component with clickable header

--- src/Pages/Position/getPositionById.tsx
+++ src/Pages/Position/getPositionById.tsx
@@ -1,11 +1,9 @@
 import { usePositionStore } from "../../state/store";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { Image } from "../../components";
 
 export default function () {
-  const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
   const { position, getOnePosition } = usePositionStore();
 
@@ -16,12 +14,18 @@ export default function () {
   });
 
   const back = () => {
-    navigate("/positions");    
+    window.history.back();  
   };
 
   return (
     <form className="flex items-center justify-center p-4 m-4">
-      <div className="flex flex-col w-full max-w-5xl p-6 space-y-6 bg-white border border-gray-400 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-6">
+      <div className="relative flex flex-col w-full max-w-5xl p-6 space-y-6 bg-white border border-gray-400 rounded-lg shadow-md md:flex-row md:space-y-0 md:space-x-6">
+      <h3
+          onClick={back}
+          className='absolute m-1 text-3xl transition-all duration-500 cursor-pointer top-1 left-1 hover:text-gray-700'
+        >
+          <i className='fa-solid fa-arrow-left'></i>
+        </h3>
         <div className="hidden w-full mr-12 md:w-1/2 md:block">
         <Image/>
         </div>

183d1e7681587b87fabab48527955ab5a37626a7
Jay200123 <kenken.ayonon@gmail.com>
2024-12-19 21:55:34 +0800

    Update AdminNavbar styles for improved text visibility

--- src/components/Navbar/AdminNavbar.tsx
+++ src/components/Navbar/AdminNavbar.tsx
@@ -117,7 +117,7 @@ export default function () {
           </li>
           <li
             onClick={borrow}
-            className="p-2 m-2 text-sm transition duration-300 rounded cursor-pointer hover:bg-gray-700 hover:text-white md:text-sm"
+            className="p-2 m-2 text-sm text-gray-700 transition duration-300 rounded cursor-pointer hover:bg-gray-700 hover:text-white md:text-sm"
           >
            <i className="mr-1 fa-solid fa-handshake"></i>Borrow Items
           </li>
@@ -197,7 +197,7 @@ export default function () {
           {user ? (
             <li
               onClick={handleLogout}
-              className="p-2 text-sm text-black transition-all duration-500 rounded-sm cursor-pointer hover:bg-gray-600 hover:text-white"
+              className="p-2 text-sm text-gray-700 transition-all duration-500 rounded-sm cursor-pointer hover:bg-gray-600 hover:text-white"
             >
               <i className="m-1 fa-solid fa-arrow-right-from-bracket"></i> Sign
               Out

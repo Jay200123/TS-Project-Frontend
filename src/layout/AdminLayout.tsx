10afde23d671213023058cb9dc34e31e53711c4b
Jay200123 <kenken.ayonon@gmail.com>
2024-12-19 21:33:47 +0800

    Refactor AdminLayout to simplify container styles in the main content area

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -5,7 +5,7 @@ export default function () {
     <>
       <main className='flex flex-col items-center justify-between min-w-full min-h-screen'>
         <AdminNavbar />
-        <div className='p-4 bg-transparent rounded-lg w-full sm:p-6 lg:p-8 max-w-full max-h-full md:w-[95%] lg:w-[1250px] xl:w-[80%]'>
+        <div className='max-w-full max-h-full'>
           <Outlet />
         </div>
         <Footer />

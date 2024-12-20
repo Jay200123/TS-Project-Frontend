8855b2e2131ea888b643608ecf084d756690c437
Jay200123 <kenken.ayonon@gmail.com>
2024-12-20 11:00:04 +0800

    Refactor position state management to improve create, update, and delete functionality

--- src/state/position.ts
+++ src/state/position.ts
@@ -3,7 +3,6 @@ import { PositionState } from "../interface";
 import api from "./api";
 import { PATH } from "../constants";
 
-
 export const usePositionStore = create<PositionState>((set) => ({
   positions: [],
   position: null,
@@ -20,7 +19,10 @@ export const usePositionStore = create<PositionState>((set) => ({
 
   getOnePosition: async (id: string) => {
     const res = await api.get(
-      `${import.meta.env.VITE_API_URI}${PATH.POSITION_ID_ROUTE.replace(":id", id)}`
+      `${import.meta.env.VITE_API_URI}${PATH.POSITION_ID_ROUTE.replace(
+        ":id",
+        id
+      )}`
     );
 
     set({
@@ -35,24 +37,44 @@ export const usePositionStore = create<PositionState>((set) => ({
   createPosition: async (formData: FormData) => {
     const res = await api.post(
       `${import.meta.env.VITE_API_URI}${PATH.POSITIONS_ROUTES}`,
-      formData);
-    set({ position: res.data.details, loading: false, error: null });
+      formData
+    );
+    set((state) => ({
+      positions: [...state.positions, res.data.details],
+      loading: false,
+      error: null,
+    }));
     return res.data.details;
   },
   updatePositionById: async (id: string, formData: FormData) => {
     const res = await api.patch(
-      `${import.meta.env.VITE_API_URI}${PATH.EDIT_POSITION_ROUTE.replace(":id", id)}`,
-      formData);
+      `${import.meta.env.VITE_API_URI}${PATH.EDIT_POSITION_ROUTE.replace(
+        ":id",
+        id
+      )}`,
+      formData
+    );
 
-    set({ position: res.data.details, loading: false, error: null });
-
+    set((state) => ({
+      positions: state.positions.map((p) =>
+        p._id === id ? res.data.details : p
+      ),
+      loading: false,
+    }));
     return res.data.details;
   },
   deletePositionById: async (id: string) => {
     const res = await api.delete(
-      `${import.meta.env.VITE_API_URI}${PATH.POSITION_ID_ROUTE.replace(":id", id)}`,
+      `${import.meta.env.VITE_API_URI}${PATH.POSITION_ID_ROUTE.replace(
+        ":id",
+        id
+      )}`
     );
-    set({ position: res.data.details, loading: false, error: null });
+    set((state) => ({
+      positions: state?.positions?.filter((p) => p?._id !== id),
+      loading: false,
+      error: null,
+    }));
     return res.data.details;
   },
 }));
